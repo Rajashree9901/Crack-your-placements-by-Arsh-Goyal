a0ddce14a3b87f71dc8e5ad18dc8bf4984ca153f
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-17 14:17:21 +0530

    Update SubarrayProduct.cpp

--- Day-3/SubarrayProduct.cpp
+++ Day-3/SubarrayProduct.cpp
@@ -42,3 +42,53 @@ int main() {
     }
     return 0;
 }
+
+
+// Solution 2: Using Sliding window protocol- O(n) approach
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        if(k<=1)
+        return 0;
+        
+        int c=0;
+        long long m=1;
+        
+        int l=0,r=0;
+        
+        while(r<n)
+        {
+            m*=a[r];
+            while(m>=k and l<r)
+            {
+                m/=a[l];
+                l++;
+            }
+            
+            if(m<k)
+            {c+=r-l+1;}
+            r++;
+        }
+        return c;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
