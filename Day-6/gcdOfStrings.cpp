f1a2f47bce4e1a33a0d25b0bb639ccc902621875
Rajashree9901 <58505906+Rajashree9901@users.noreply.github.com>
2022-05-21 14:17:34 +0530

    Create gcdOfStrings.cpp

---
+++ Day-6/gcdOfStrings.cpp
@@ -0,0 +1,37 @@
+// Problem link- https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+// Solution-1:
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1.length()<str2.length())
+        {
+            return gcdOfStrings(str2,str1);
+        }
+        else if(str1.find(str2)!=0)
+            return "";
+        else if(str2=="")
+            return str1;
+        else
+        {
+            return gcdOfStrings(str1.substr(str2.length()),str2);
+        }
+    }
+};
+
+// Solution-2
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if((str1+str2)!=(str2+str1))
+            return "";
+        else if(str1==str2)
+            return str1;
+        else if(str1.length()>str2.length())
+        {
+            return gcdOfStrings(str1.substr(str2.length()),str2);
+        }
+        else
+            return gcdOfStrings(str2,str1);
+    }
+};
